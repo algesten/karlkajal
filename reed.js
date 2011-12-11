fd220451b53029a59a72b20bcba28f3d40aab2ce
Martin Algesten <martin@algesten.se>
2011-12-12 00:06:43 +0100

    added if around metadata

--- reed.js
+++ reed.js
@@ -375,8 +375,9 @@ reed.getMetadata = function(title, callback) {
 		
 	//first hit redis
 	client.hget(key, "metadata", function(err, metadata) {
-    console.log(metadata);
-		metadata = JSON.parse(metadata);
+    if (typeof(metadata) !== "undefined" && metadata != null) {
+      metadata = JSON.parse(metadata);
+    }
 		if (typeof(metadata) !== "undefined" && metadata != null && Object.keys(metadata).length > 0) {
 			if (typeof callback !== "undefined")
 				callback(null, metadata);
